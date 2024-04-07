96314d333a770fd7c2421075e0886a971dbd9660
Luis Miguel Aguirre Serna <miguiluis90@gmail.com>
2024-04-07 15:06:49 -0500

    config departments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useState,useEffect} from  'react';//the useState is the constructor.
+import Axios from 'axios';
 
 function App() {
+  const[counter,setCounter]= useState(0); //give a name to the variable and a set , that is a component that let me change the value of the variable
+  const[department, setDepartments]=useState([]);
+  const[selectedDepartment, setSelectedDepartment]=useState('');
+  useEffect(()=>{
+    const obtainDepartments = async () =>{
+      const response = await Axios({
+        url: `http://localhost:1337/api/listdepartment`
+      });
+      const listDeparments = Object.keys(response.data).map(i => response.data[i]);
+      console.log(listDeparments);
+      setDepartments(listDeparments.flat());
+    }
+
+    obtainDepartments();
+    
+  },[]);
+  function handleClick(){
+    setCounter(counter+1);
+  }
+  function handleDecrease(){
+    setCounter(counter-1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Counter {counter}</label>
+      <div>
+      <button onClick={handleClick}>Increase Counter</button>
+      </div>
+     <div>
+      <button onClick={handleDecrease}>Decrease Counter</button>
+    </div>
     </div>
   );
 }
